5f81be329ec48319fae4d16dd8693749a806eeb6
Tyler Kuromiya Parker <tylerkkp@gmail.com>
2020-12-15 23:07:51 -0800

    Advent of Code 2020 day 9 part 2 solution

--- solutions/day_9/daynine.py
+++ solutions/day_9/daynine.py
@@ -12,8 +12,6 @@ with open('adventofcode2020_day9.txt') as inputfile:
     for line in inputfile:
         results.append(int(line.strip()))
 
-print(results[0:25])
-
 res_len = len(results)
 
 for i in range(25, res_len):
@@ -23,5 +21,33 @@ for i in range(25, res_len):
         if results[i] - testlist[j] in testlist:
             passtest += 1
     if passtest == 0:
-        print(results[i])
+        print('part 1 solution:', results[i])
         break
+
+revres = results[::-1]
+breakloc = revres.index(2089807806)
+revres = revres[breakloc:]
+revres = revres[1:]
+
+global testsize
+testsize = 2
+tlist = []
+
+
+for i in range(len(revres)):
+    tlist = []
+    if revres[i] > 2089807806:
+        continue
+    for j in range(testsize):
+        tlist.append(revres[i+j])
+    listsum = sum(map(int, tlist))
+    if listsum == 2089807806:
+        finallist = tlist
+        break
+    elif listsum < 2089807806:
+        testsize += 1
+    else:
+        continue
+
+finallist = sorted(finallist)
+print('part 2 solution:', finallist[0] + finallist[-1])
